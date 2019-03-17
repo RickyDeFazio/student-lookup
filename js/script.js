e1785172e8849519690b34f56621365ee8f341cc
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-03-17 13:46:56 -0400

    Exceeds Expectations Complete

--- js/script.js
+++ js/script.js
@@ -6,11 +6,17 @@ FSJS project 2 - List Filter and Pagination
 const studentList = document.querySelector('.student-list');
 const listItems = studentList.children;
 const page = document.querySelector('.page');
+
+
+// Result
+const result = document.createElement('h1');
+page.appendChild(result);
+
 let numberOfPages = 0;
 
-/* 
-  search functionality added here
-*/
+/**
+ * Add Search Elements
+ */
 
 const pageHeader = document.querySelector('.page-header');
 const studentSearch = document.createElement('div');
@@ -21,21 +27,21 @@ const input = document.createElement('input');
 studentSearch.appendChild(input);
 input.placeholder = "Search for students...";
 
-const button = document.createElement('button');
-button.textContent = "Search";
-studentSearch.appendChild(button);
+const submitButton = document.createElement('button');
+submitButton.textContent = "Search";
+studentSearch.appendChild(submitButton);
 
 
 /*
   Hides all students except for the 10 on the chosen page.
 */
 
-function showPage(listOfStudents, page) {
-  for (let i = 0; i < listOfStudents.length; i++) {
+function showPage(list, page) {
+  for (let i = 0; i < list.length; i++) {
     if (i >= (page * 10 - 10) && i <= (page * 10) - 1) {
-      listOfStudents[i].style.display = 'block';
+      list[i].style.display = 'block';
     } else {
-      listOfStudents[i].style.display = "none";
+      list[i].style.display = "none";
     }
   }
 }
@@ -46,11 +52,12 @@ showPage(listItems, 1);
 
 /*
   1. Iterates through list to determine how many pages are needed.
-  2. Creates div and ul to append page links.
-  3. Creates an li and a tag for each page.
-  4. Adds event listener for each page link.
-  5. Removes 'active' class on previously clicked page link.
-  6. Adds 'active' class to most recent page clicked.
+  2. Checks and removes current pagination elements.
+  3. Creates div and ul to append new page links.
+  4. Creates an li and a tag for each page.
+  5. Adds event listener for each page link.
+  6. Removes 'active' class on previously clicked page link.
+  7. Adds 'active' class to most recent page clicked.
 */
 
 function appendPageLinks(list) {
@@ -58,6 +65,11 @@ function appendPageLinks(list) {
     numberOfPages = i / 10;
   }
   numberOfPages = Math.ceil(numberOfPages);
+  
+  if (document.querySelector('.pagination')) {
+    deletePag = document.querySelector('.pagination')
+    deletePag.parentNode.removeChild(deletePag);
+  }
 
   const paginationDiv = document.createElement('div');
   paginationDiv.className = "pagination";
@@ -71,9 +83,8 @@ function appendPageLinks(list) {
     const a = document.createElement('a');
     li.appendChild(a);
     a.textContent = i;
-    
     a.addEventListener('click', (e) => {
-      showPage(listItems, i);
+      showPage(list, i);
       const allATags = document.querySelectorAll('a');
       for (let i = 0; i < allATags.length; i++) {
         allATags[i].classList.remove("active");
@@ -83,4 +94,41 @@ function appendPageLinks(list) {
   }
 }
 
+// Shows the initial page links when the page first loads
 appendPageLinks(listItems);
+
+
+/*
+ * 1. Searches for students by name and email.
+ * 2. Shows students matching input.
+ * 3. Hides students that don't match.
+ * 4. Displays 'no results found' if no student matches.
+ */
+
+function searchForStudents(list){
+  const studentsFound = [];
+  for (let i = 0; i < list.length; i++) {
+    const studentDetails = document.querySelectorAll('.student-details');
+    const h3 = studentDetails[i].children[1];
+    const email = document.querySelectorAll('.email');
+    if (h3.textContent.includes(input.value) || email[i].textContent.includes(input.value)) {
+      studentsFound.push(list[i]);
+    } else {
+      list[i].style.display = 'none';
+    }
+  }
+  appendPageLinks(studentsFound);
+  showPage(studentsFound, 1);
+  if (studentsFound.length === 0 && input.value.length > 0) {
+    result.textContent = "No results found";
+  } else {
+    result.textContent = "";
+  }
+}
+
+input.addEventListener('keyup', () => {
+  searchForStudents(listItems);
+});
+submitButton.addEventListener('click', () => {
+  searchForStudents(listItems);
+});
