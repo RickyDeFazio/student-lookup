83cd600f1169508b6edf996744c6c9d42f914c34
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2020-09-24 11:25:37 -0400

    refactored (still needs more documentation)

--- js/script.js
+++ js/script.js
@@ -1,118 +1,169 @@
-const studentList = document.querySelector('.student-list');
-const listItems = studentList.children;
+// Global Variables
+
 const page = document.querySelector('.page');
+const pageHeader = document.querySelector('.page-header');
+const studentList = document.createElement('ul');
+const listItems = studentList.children;
+const result = document.createElement('h2');
+const studentSearch = document.createElement('div');
+const input = document.createElement('input');
 let numberOfPages = 0;
 
-const result = document.createElement('h1');
-page.appendChild(result);
-
-/**
- * Add Search Elements
- */
-
-const pageHeader = document.querySelector('.page-header');
-const studentSearch = document.createElement('div');
+studentList.className = 'student-list';
 studentSearch.className = "student-search";
-pageHeader.appendChild(studentSearch);
-
-const input = document.createElement('input');
-studentSearch.appendChild(input);
 input.placeholder = "Search for students...";
 
+// Append elements to DOM
+page.appendChild(studentList);
+page.appendChild(result);
+pageHeader.appendChild(studentSearch);
+studentSearch.appendChild(input);
 
-/*
-  Hides all students except for the 10 on the chosen page.
-*/
+/**
+ *
+ */
+async function main() {
+  await populateStudents();
+  appendPageLinks(listItems);
+  showPage(listItems, 1);
+  document.querySelector('.pagination a').classList.add('active');
+}
 
-function showPage(list, page) {
-  for (let i = 0; i < list.length; i++) {
-    if (i >= (page * 10 - 10) && i <= (page * 10) - 1) {
-      list[i].style.display = 'block';
-    } else {
-      list[i].style.display = "none";
-    }
+/**
+ *
+ */
+async function getStudents() {
+  try {
+    const res = await fetch('https://randomuser.me/api/?results=100');
+    return res.json();
+  }
+  catch(error) {
+    console.error(error);
   }
 }
 
-// Shows the first 10 students when the page initially loads
-showPage(listItems, 1);
+/**
+ *
+ */
+async function populateStudents() {
+  const data = await getStudents();
+  const { results: users } = data;
+  let html = ``;
 
+  users.forEach(user => {
+    html += /*html*/ `
+      <li class="student-item cf">
+        <div class="student-details">
+          <img class="avatar" src="${user.picture.thumbnail}">
+          <h3>${user.name.first} ${user.name.last}</h3>
+            <span class="email">${user.email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined Recently</span>
+        </div>
+      </li>
+    `;
+  })
 
-/*
-  1. Iterates through list to determine how many pages are needed.
-  2. Checks and removes current pagination elements.
-  3. Creates div and ul to append new page links.
-  4. Creates an li and a tag for each page.
-  5. Adds event listener for each page link.
-  6. Removes 'active' class on previously clicked page link.
-  7. Adds 'active' class to most recent page clicked.
-*/
+  studentList.innerHTML = html;
+}
 
-function appendPageLinks(list) {
-  for (let i = 0; i <= list.length; i++){
-    numberOfPages = i / 10;
-  }
-  numberOfPages = Math.ceil(numberOfPages);
-  
-  if (document.querySelector('.pagination')) {
-    deletePag = document.querySelector('.pagination')
-    deletePag.parentNode.removeChild(deletePag);
-  }
+/**
+ * Hides all students except for the 10 on the chosen page.
+ * @param {HTMLCollection} students List of students
+ * @param {Number} page Current page
+ */
+function showPage(students, page) {
+  Array.from(students).forEach((student, i) => {
+    if (i >= (page * 10 - 10) && i <= (page * 10) - 1) {
+      student.style.display = 'block';
+    } else {
+      student.style.display = "none";
+    }
+  })
+}
 
+/**
+ * - Iterates through list to determine how many pages are needed.
+ * - Checks and removes current pagination elements.
+ * - Creates div and ul to append new page links.
+ * - Creates an li and a tag for each page.
+ * - Adds event listener for each page link.
+ * - Removes 'active' class on previously clicked page link.
+ * - Adds 'active' class to most recent page clicked.
+ * @param {HTMLCollection} students
+ */
+function appendPageLinks(students) {
+  const paginationEl = document.querySelector('.pagination');
   const paginationDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+
   paginationDiv.className = "pagination";
   page.appendChild(paginationDiv);
-  const ul = document.createElement('ul');
   paginationDiv.appendChild(ul);
+
+  for (let i = 0; i <= students.length; i++){
+    numberOfPages = i / 10;
+  }
+
+  numberOfPages = Math.ceil(numberOfPages);
+
+  if (paginationEl) {
+    paginationEl.parentNode.removeChild(paginationEl);
+  }
 
   for (let i = 1; i <= numberOfPages; i++) {
     const li = document.createElement('li');
-    ul.appendChild(li);
     const a = document.createElement('a');
-    li.appendChild(a);
+
     a.textContent = i;
+    ul.appendChild(li);
+    li.appendChild(a);
+
     a.addEventListener('click', (e) => {
-      showPage(list, i);
-      const allATags = document.querySelectorAll('a');
-      for (let i = 0; i < allATags.length; i++) {
-        allATags[i].classList.remove("active");
-  }
+      const activeTags = document.querySelectorAll('a.active');
+      showPage(students, i);
+      activeTags.forEach(a => {
+        a.classList.remove("active");
+      })
       e.target.className = "active";
     });
   }
 }
 
-// Shows the initial page links when the page first loads
-appendPageLinks(listItems);
-
-/*
- * 1. Searches for students by name and email.
- * 2. Shows students matching input.
- * 3. Hides students that don't match.
- * 4. Displays 'no results found' if no student matches.
+/**
+ * - Searches for students by name and email.
+ * - Shows students matching input.
+ * - Hides students that don't match.
+ * - Displays 'no results found' if no student matches.
+ * @param {HTMLCollection} students
  */
-
-function searchForStudents(list) {
+function searchForStudents(students) {
   const studentsFound = [];
-  for (let i = 0; i < list.length; i++) {
+
+  Array.from(students).forEach((student, i) => {
     const studentDetails = document.querySelectorAll('.student-details');
-    const h3 = studentDetails[i].children[1];
+    const name = studentDetails[i].children[1];
     const email = document.querySelectorAll('.email');
-    if (h3.textContent.toLowerCase().includes(input.value.toLowerCase()) || email[i].textContent.toLowerCase().includes(input.value.toLowerCase())) {
-      studentsFound.push(list[i]);
-    } else {
-      list[i].style.display = 'none';
-    }
-  }
+    const hasName = name.textContent.toLowerCase().includes(input.value.toLowerCase());
+    const hasEmail = email[i].textContent.toLowerCase().includes(input.value.toLowerCase());
+
+    hasName || hasEmail ? studentsFound.push(student) : student.style.display = 'none';
+  })
+
   appendPageLinks(studentsFound);
   showPage(studentsFound, 1);
+
   if (studentsFound.length === 0 && input.value.length > 0) {
     result.textContent = "No results found";
   } else {
     result.textContent = "";
+    document.querySelector('.pagination a').classList.add('active');
   }
 }
 
 input.addEventListener('keyup', () => {
   searchForStudents(listItems);
 });
+
+main();
